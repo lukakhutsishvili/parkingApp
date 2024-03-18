941f4063c23c653b1e76b0a1deb4b746a3549a8b
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-18 22:28:21 +0400

    feat

--- src/config/sql.js
+++ src/config/sql.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 
 export const createTable = async () => {
   return await pool.query(
-    "CREATE TABLE IF NOT EXISTS customer(id SERIAL PRIMARY KEY, costumername TEXT, costumerpassword TEXT)"
+    "CREATE TABLE IF NOT EXISTS customer(id SERIAL PRIMARY KEY, costumername TEXT, costumerpassword INT)"
   );
 };
 
