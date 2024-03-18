6afd52517a8f69c0a49f27453f931c048a4e090c
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-18 22:49:25 +0400

    feat

--- src/config/sql.js
+++ src/config/sql.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 
 export const createTable = async () => {
   return await pool.query(
-    "CREATE TABLE IF NOT EXISTS customer(id SERIAL PRIMARY KEY, name TEXT, pass INT)"
+    "CREATE TABLE IF NOT EXISTS customer(id SERIAL PRIMARY KEY, name TEXT, price INT)"
   );
 };
 
--- src/server.js
+++ src/server.js
@@ -27,10 +27,10 @@ async function init() {
     });
     app.post("/api/post", async (req, res) => {
       try {
-        const { name, pass } = req.body;
+        const { name, price } = req.body;
         const resultQuery = await pool.query(
-          "INSERT INTO customer(name, pass) VALUES($1, $2)",
-          [name, pass]
+          "INSERT INTO customer(name, price) VALUES($1, $2)",
+          [name, price]
         );
         const rows = resultQuery.rows;
         return res.status(201).json(rows);
