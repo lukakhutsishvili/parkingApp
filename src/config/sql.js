e14730689fd5827ee1ad08694f51342e70e1b472
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-18 17:34:17 +0400

    finished set up

---
+++ src/config/sql.js
@@ -0,0 +1,15 @@
+import pgk from "pg";
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-cns3sbq0si5c73c235sg-a",
+  port: 5432,
+  database: "parkingapp",
+  user: "parkingapp_user",
+  password: "V5mn64dabA0IsoGUGdVTc5Bgmn5I54nP",
+});
+
+export const createTable = async () => {
+  return await pool.query("CREATE TABLE IF NOT EXISTS costumer(id SERIAL PRIMARY KEY, title TEXT, price INT)");
+};
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { createTable } from "./config/sql";
+
+const app = express();
+
+async function init() {
+  try {
+    await createTable();
+    serverStart();
+  } catch (error) {
+    console.log(error);
+  }
+
+  function serverStart() {
+    app.get("/", (req, res) => {
+      return res.status(200).json({ message: "works" });
+    });
+    app.listen(3000);
+  }
+}
