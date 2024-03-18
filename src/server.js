5b8d31eab14017ec3e781ef4aa4681ab98e98c4c
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-18 19:48:31 +0400

    feat: create new costumer post request

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pool, { createTable } from "./config/sql.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -12,11 +13,25 @@ async function init() {
   }
 
   function serverStart() {
+    app.use(bodyParser.json());
     app.get("/", async (_, res) => {
       try {
         const resultQuery = await pool.query("SELECT * FROM costumer");
         const rows = resultQuery.rows;
         return res.status(200).json(rows);
+      } catch (error) {
+        return res.status(401).json(error);
+      }
+    });
+    app.post("/", async (req, res) => {
+      try {
+        const { username, password } = req.body;
+        const resultQuery = await pool.query(
+          "INSERT INTO TABLE  costumer(username, password) VALUES($1, $2)",
+          [username, password]
+        );
+        const row = resultQuery.rows[0];
+        return res.status(201).json(row);
       } catch (error) {
         return res.status(401).json(error);
       }
